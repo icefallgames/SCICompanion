3ad8642450a0a8d22e47d467773ee2204be19b3f
icefallgames <Philip.Fortier@gmail.com>
2015-09-27 17:22:03 -0700

    - reduce size of resources (reduce binary size)

--- SCICompanion/stdafx.h
+++ SCICompanion/stdafx.h
@@ -20,18 +20,12 @@
 #include <afxext.h>         // MFC extensions
 
 
-
-
-
 #ifndef _AFX_NO_OLE_SUPPORT
 #include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
 #endif
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>             // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
-
-#include <afxcontrolbars.h>     // MFC support for ribbons and control bars
-
 
 #include <afxcview.h>       // CListView
 
